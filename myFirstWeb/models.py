70d84c8aea1f4712f0dd50486f47bf72c4649e9d
LucienArraki <corvinlucien@gmail.com>
2017-07-27 20:17:14 +0200

    Ustawianie css"

--- myFirstWeb/models.py
+++ myFirstWeb/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class information(models.Model):
+    animalName = models.CharField(max_length=120)
+    startDate = models.DateTimeField()
+    endDate = models.DateTimeField()
+
+    def __unicode__(self):
+        return self.animalName
+
+    def __str__(self):
+        return  self.animalName
