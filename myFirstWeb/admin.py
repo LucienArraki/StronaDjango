5fabeea01ca51cbce5d11d51bf52a6e24987b081
LucienArraki <corvinlucien@gmail.com>
2017-07-31 16:51:23 +0200

    Panel administratora i poprawa bazy danych

--- myFirstWeb/admin.py
+++ myFirstWeb/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Animal_category, Animal_type
 # Register your models here.
+
+class Animal_admin(admin.ModelAdmin):
+    list_display = ('animal_type','animal_category','start_date','endDate')
+
+admin.site.register(Animal_category,Animal_admin)
+
+admin.site.register(Animal_type)
--- myFirstWeb/models.py
+++ myFirstWeb/models.py
@@ -1,14 +1,26 @@
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
 
-class information(models.Model):
-    animalName = models.CharField(max_length=120)
-    startDate = models.DateTimeField()
-    endDate = models.DateTimeField()
+class Animal_type(models.Model):
+    type = models.CharField(max_length=120)
 
     def __unicode__(self):
-        return self.animalName
+        return self.type
 
     def __str__(self):
-        return  self.animalName
+        return  self.type
+
+
+class Animal_category(models.Model):
+    animal_type = models.ForeignKey(Animal_type)
+    animal_category = models.CharField(max_length=120)
+    start_date = models.DateField()
+    endDate = models.DateField()
+
+    def __unicode__(self):
+        return self.animal_category
+
+    def __str__(self):
+        return  self.animal_category
